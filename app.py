1f2bb60ae0f22211f4fa09ccb3c8fe14f0314e9c
skarthas <stacy.karthas@gmail.com>
2017-01-02 23:45:56 +0000

    First Working Application

--- app.py
+++ app.py
@@ -1,14 +1,68 @@
 from flask import Flask, render_template, request, redirect
+import requests
+import simplejson as json
+from bokeh.plotting import figure,show,ColumnDataSource
+from bokeh.embed import components
+from bokeh.resources import INLINE
+import pandas as pd
+import numpy as np
+from datetime import datetime,timedelta
+import os
 
 app = Flask(__name__)
+
+app.vars={}
 
 @app.route('/')
 def main():
   return redirect('/index')
 
-@app.route('/index')
-def index():
-  return render_template('index.html')
+@app.route('/index',methods=['Get','POST'])
+def indexes():
+  if request.method == 'GET':
+  	return render_template('index.html')
+  else: 
+	app.vars['tickername'] = request.form['ticker']
+	app.vars['close'] = request.form['cprice']
+	app.vars['aclose'] = request.form['acprice']
+	app.vars['open'] = request.form['oprice']
+	app.vars['aopen'] = request.form['aoprice']
+	return redirect('/graphing')	
 
+@app.route('/graphing',methods=['GET'])
+def graphing():
+  now = datetime.now()-timedelta(days=3)
+  past = now-timedelta(days=33)
+  api_url='https://www.quandl.com/api/v3/datatables/WIKI/PRICES.json?ticker=%s&qopts.columns=date,open,close,adj_open,adj_close&date.gte=%s&date.lt=%s&api_key=zTJsP31gZnkKyDXYG5DD' % (app.vars['tickername'],past.strftime("%Y%m%d"),now.strftime("%Y%m%d"))
+  session = requests.Session()
+  session.mount('http://', requests.adapters.HTTPAdapter(max_retries=5))
+  raw_data = session.get(api_url)
+  data = raw_data.json()
+  pdf = pd.DataFrame(data['datatable']['data'],columns=['dates','open','close','adj_open','adj_close'])
+  pdf['dates']=pd.to_datetime(pdf['dates'])
+  print(pdf.dtypes)
+  ts = pd.Series(np.random.randn(1000), index=pd.date_range('1/1/2000', periods=1000))
+  plot = figure(title='Data from Quandle WIKI set',x_axis_label='date',x_axis_type='datetime')
+  if(app.vars['close']=='on'):
+	plot.line(x=pdf['dates'],y=pdf['close'],legend='%s:Close'%app.vars['tickername'],color="blue") 
+
+  if(app.vars['aclose']=='on'):  
+        plot.line(x=pdf['dates'],y=pdf['adj_close'],legend='%s:Adj. Close'%app.vars['tickername'],color="green")  
+ 
+  if(app.vars['open']=='on'):
+        plot.line(x=pdf['dates'],y=pdf['open'],legend='%s:Open'%app.vars['tickername'],color="orange")
+
+  if(app.vars['aopen']=='on'):
+        plot.line(x=pdf['dates'],y=pdf['adj_open'],legend='%s:Adj. Open'%app.vars['tickername'],color="red")
+
+
+  js_resources = INLINE.render_js()
+  css_resources = INLINE.render_css()
+  script, div = components(plot)
+
+  return render_template('graph2.html',plot_script=script,plot_div=div, js_resources=js_resources, css_resources=css_resources, ticker=app.vars['tickername'])
+	
+
+  
 if __name__ == '__main__':
-  app.run(port=33507)
+  app.run(port=33507,debug=True)
